30929e6b1712f8cb6c1ca37e836f7d73edc26322
ignacyr <ignacy.rogatty@gmail.com>
2022-04-05 13:52:17 +0200

    added SVD

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA, TruncatedSVD
 from random_points import *
 
-plot_limits = [-5, 5]
+limit = 3
+plot_limits = [-limit, limit]
 n = 10
 random_points_3d, random_points_2d = random_points_gen(n)
 
+# 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(random_points_3d[:, 0], random_points_3d[:, 1], random_points_3d[:, 2])
-
+plt.title('3D')
+ax.set_xlim3d(plot_limits)
+ax.set_ylim3d(plot_limits)
+ax.set_zlim3d(plot_limits)
 
 pca = PCA(n_components=2)
-points_2d = pca.fit_transform(random_points_3d)
+points_2d_pca = pca.fit_transform(random_points_3d)
 
-print(random_points_2d.shape)
-print(random_points_3d.shape)
-print(points_2d.shape)
+svd = TruncatedSVD(n_components=2)
+points_2d_svd = svd.fit_transform(random_points_3d)
 
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.scatter(points_2d[:, 0], points_2d[:, 1])
-plt.xlim(plot_limits)
-plt.ylim(plot_limits)
-plt.grid()
-plt.title('PCA')
 
+# Original points on 2d plane plot
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.scatter(random_points_2d[:, 0], random_points_2d[:, 1])
@@ -33,5 +31,25 @@ plt.xlim(plot_limits)
 plt.ylim(plot_limits)
 plt.grid()
 plt.title('Original')
+
+# PCA points on 2d plane plot
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.scatter(points_2d_pca[:, 0], points_2d_pca[:, 1])
+plt.xlim(plot_limits)
+plt.ylim(plot_limits)
+plt.grid()
+plt.title('PCA')
+
+# SVD points on 2d plane plot
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.scatter(points_2d_svd[:, 0], points_2d_svd[:, 1])
+plt.xlim(plot_limits)
+plt.ylim(plot_limits)
+plt.grid()
+plt.title('SVD')
+
+plt.show()
 
 
